3e0790134b3b648adf892c05ec7e498812073fd0
mjbeauchamp <monica.j.beauchamp@gmail.com>
2025-09-10 12:31:56 -0600

    Basic auth0 setup

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import express from "express";
 import { connectDB } from "./config/db.ts";
 import commonGearRoutes from "./routes/commonGear.routes.ts";
 import cors from "cors";
+import { auth } from 'express-oauth2-jwt-bearer'
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
+  tokenSigningAlg: process.env.AUTH0_TOKEN_SIGN_ALG
+});
 
 connectDB();
 
@@ -24,7 +33,7 @@ app.use(
 
 app.use(express.json());
 
-app.use("/api/commonGear", commonGearRoutes);
+app.use("/api/commonGear", jwtCheck, commonGearRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
