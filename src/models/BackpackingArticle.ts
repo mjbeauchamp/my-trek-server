170f0d320c4ef4eb3a5c00be9b6ea158e03aaaba
mjbeauchamp <monica.j.beauchamp@gmail.com>
2025-12-08 14:19:19 -0600

    Updating BackpackingArticle properties

--- src/models/BackpackingArticle.ts
+++ src/models/BackpackingArticle.ts
@@ -3,8 +3,8 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IBackpackingArticle extends Document {
   title: string;
   author: string;
-  date: string;
-  image: string;
+  imageUrl: string;
+  imageAlt: string;
   content: string[];
 }
 
@@ -12,8 +12,8 @@ const BackpackingArticleSchema: Schema = new Schema(
   {
     title: { type: String, required: true },
     author: { type: String, required: true },
-    date: { type: String, required: true },
-    image: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    imageAlt: { type: String, required: true },
     content: { type: [String], required: true },
   },
   { collection: "backpackingArticles" }
