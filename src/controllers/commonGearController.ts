cd1f0c9dc72073e4cfea0aca364752acaa2e4d0f
mjbeauchamp <monica.j.beauchamp@gmail.com>
2025-12-20 14:26:37 -0600

    Updating error handling on getCommonGearItems

--- src/controllers/commonGearController.ts
+++ src/controllers/commonGearController.ts
@@ -3,8 +3,15 @@ import CommonGear from '../models/CommonGear.js';
 
 export async function getCommonGearList(req: Request, res: Response) {
     try {
-        const gear = await CommonGear.find();
-        res.json(gear);
+        const list = await CommonGear.find();
+
+        if (!list) {
+            return res
+                .status(404)
+                .json({ message: 'There was an issue fetching the common gear list' });
+        }
+
+        res.json(list);
     } catch (err) {
         console.error('Error fetching common gear items:', err);
         res.status(500).json({ message: 'Server error fetching common gear items' });
